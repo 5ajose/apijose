1553e71e8addd99b0ef5ff216b72f6e3a66403cc
5ajose <jaquintoaguilar@gmail.com>
2025-02-06 21:27:06 -0500

    product

--- routes/router.js
+++ routes/router.js
@@ -14,3 +14,20 @@ router.post('/inicioDeSesion', usersControllers.inicioDeSesion)
 router.get('/users', middlewareJWT.verificacionDeToken , usersControllers.getUsers)
 
 module.exports =  router
+
+const productController = require('../controllers/product.controller');
+const loginProductController = require('../controllers/loginProduct.controller')
+// const middlewareJwt  = require('../middleware/jwt');
+
+
+//***********rutas para manejar los productos***********
+router.get('/showProduct',productController.allProducts);
+router.get('/showOne/:id',productController.getOneProduct);
+router.post('/addProduct', productController.add); // verificar middleware para agregar en angular = middlewareJwt.verificacionToken,
+router.put('/upDate/:id',productController.modify);
+router.delete('/delete/:id',productController.deleteProduct);
+
+//******** ingreso de admin con token******** */
+router.post('/loginUser',loginProductController.login)
+
+module.exports = router;
